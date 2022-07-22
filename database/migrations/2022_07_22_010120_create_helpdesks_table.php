79e7d8e9627b4d4c46ede47d7cc1b913b4a3ccf3
erza <58721335+erzaffasya@users.noreply.github.com>
2022-07-22 09:12:32 +0800

    + helpdesk, kategori helpdesk

---
+++ database/migrations/2022_07_22_010120_create_helpdesks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('helpdesk', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('no_hp');
+            $table->foreignId("kategori_helpdesk_id")->constrained("kategori_helpdesk")->onDelete("cascade")->onUpdate("cascade");
+            $table->string('ttd');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('helpdesk');
+    }
+};
