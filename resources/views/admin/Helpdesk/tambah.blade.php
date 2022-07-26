73f3a9e7f764d83b5945dd5f41bae9e3f2e0369d
Erza Fahmi Fasya <effasya@gmail.com>
2022-07-26 14:55:34 +0800

    Update tambah.blade.php

--- resources/views/admin/Helpdesk/tambah.blade.php
+++ resources/views/admin/Helpdesk/tambah.blade.php
@@ -107,7 +107,7 @@
                         let kategori_helpdesk_id = $('#kategori_helpdesk_id').val();
                         let keterangan = $('#keterangan').val();
                         $.ajax({
-                            url: "{{ route('Helpdesk.store') }}",
+                            url: "Helpdesk/store",
                             type: 'POST',
                             headers: {
                                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
