08850c26956cca2cc1377a9fbda0c5874b9654c4
erza <58721335+erzaffasya@users.noreply.github.com>
2022-08-10 14:50:15 +0800

    revisi dashboard controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,47 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function PerBulan($query){
+        $data = null;
+        foreach ($query as $item) {
+            if ($item->bulan == 'January') {
+                $data['January'] = $item->jumlah;
+            }
+            if ($item->bulan == 'February') {
+                $data['February'] = $item->jumlah;
+            }
+            if ($item->bulan == 'March') {
+                $data['March'] = $item->jumlah;
+            }
+            if ($item->bulan == 'April') {
+                $data['April'] = $item->jumlah;
+            }
+            if ($item->bulan == 'May') {
+                $data['May'] = $item->jumlah;
+            }
+            if ($item->bulan == 'June') {
+                $data['June'] = $item->jumlah;
+            }
+            if ($item->bulan == 'July') {
+                $data['July'] = $item->jumlah;
+            }
+            if ($item->bulan == 'August') {
+                $data['August'] = $item->jumlah;
+            }
+            if ($item->bulan == 'September') {
+                $data['September'] = $item->jumlah;
+            }
+            if ($item->bulan == 'October') {
+                $data['October'] = $item->jumlah;
+            }
+            if ($item->bulan == 'November') {
+                $data['November'] = $item->jumlah;
+            }
+            if ($item->bulan == 'December') {
+                $data['December'] = $item->jumlah;
+            }
+        }
+        return $data;
+    }
 }
--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,33 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\NSeksi;
 use App\Models\NSektor;
-use App\Models\Perizinan;
-use App\Models\Permohonan;
 use App\Models\SektorKesehatan;
 use App\Models\SektorPendidikan;
+use App\Models\SektorPerdagangan;
 use App\Models\SektorPertahanan;
-use App\Models\Terbit;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        $Terbit = Terbit::all();
-        $Terbit1 = Terbit::paginate(5);
-        $Permohonan = Permohonan::all();
-        $Permohonan1 = Permohonan::paginate(5);
-        $Perizinan = Perizinan::all();
-        $Perizinan1 = Perizinan::paginate(5);
-
         $DataSektor = NSektor::all();
         $DataSeksi = NSeksi::all();
+
         $DataPertanahan = SektorPertahanan::all();
         $DataKesehatan = SektorKesehatan::all();
         $DataPendidikan = SektorPendidikan::all();
-        $QueryTotal =  $DataPertanahan->count() + $DataKesehatan->count() + $DataPendidikan->count();
-        // dd($DataPendidikan);
-        return view('admin.index', compact('QueryTotal', 'DataPertanahan', 'DataKesehatan', 'DataPendidikan', 'DataSektor','DataSeksi','Terbit', 'Permohonan', 'Perizinan','Terbit1','Permohonan1','Perizinan1'));
+        $DataPerdagangan = SektorPerdagangan::all();
+        $QueryTotal =  $DataPertanahan->count() + $DataKesehatan->count() + $DataPendidikan->count() + $DataPerdagangan->count();
+
+        $query = SektorPerdagangan::select( DB::raw("COUNT(*) as jumlah"), DB::raw("MONTHNAME(tanggal) as bulan"))
+        ->whereYear('tanggal', date("Y"))
+        ->groupBy('bulan')->get();
+        
+        // dd($this->PerBulan($query));
+        return view('admin.index', compact('QueryTotal', 'DataPerdagangan', 'DataPertanahan', 'DataKesehatan', 'DataPendidikan', 'DataSektor','DataSeksi'));
     }
 }
