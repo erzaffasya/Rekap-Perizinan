48161ace46cef5c1c45a1d0188b0c07f60344591
erza <58721335+erzaffasya@users.noreply.github.com>
2022-08-23 16:24:53 +0800

    revisi warna navbar + sidebar

--- resources/views/admin/partials/navbar.blade.php
+++ resources/views/admin/partials/navbar.blade.php
@@ -1,4 +1,5 @@
-<nav class="navbar navbar-main navbar-expand-lg position-sticky mt-4 top-1 px-0 mx-4 shadow-none border-radius-xl z-index-sticky"
+<nav style="    color: black;
+background-color: #c5dffa;" class="navbar navbar-main navbar-expand-lg position-sticky mt-4 top-1 px-0 mx-4 shadow-none border-radius-xl z-index-sticky"
     id="navbarBlur" data-scroll="true">
     <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
